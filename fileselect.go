9206a7d60782d322a312fc864ab3c3e476ace55f
Artūras Kneižys <arturas.kneizys@narbutas.lt>
2025-02-16 19:22:07 +0200

    Changed API, added an option to parse as flag

--- fileselect.go
+++ fileselect.go
@@ -10,57 +10,70 @@ import (
 	"strings"
 )
 
-type item struct {
+type FilePath struct {
 	name        string
 	message     string
 	defaultPath string
-	response    *string
+	flagValue   *string
+	response    string
 	mustExist   bool
 }
 
+// type FilePath string
+
 var Quiet *bool
 
-var expectedItems []item
+var expectedArgs []*FilePath
 
 // Open defines a request for a file path that should exist (unless a - is entered)
-func Open(name, defaultPath, message string) *string {
-	return path(name, defaultPath, message, true)
+func Open(name, defaultPath, message string) *FilePath {
+	fp := Create(name, defaultPath, message)
+	fp.mustExist = true
+	return fp
 }
 
 // Create defines a request for a file path that doesn't have to exist
-func Create(name, defaultPath, message string) *string {
-	return path(name, defaultPath, message, false)
-}
-
-func path(name, defaultPath, message string, mustExist bool) *string {
-	response := defaultPath
-	expectedItems = append(expectedItems, item{
+func Create(name, defaultPath, message string) *FilePath {
+	it := FilePath{
 		name:        name,
 		message:     message,
 		defaultPath: defaultPath,
-		response:    &response,
-		mustExist:   mustExist,
-	})
-	return &response
+	}
+	expectedArgs = append(expectedArgs, &it)
+	return &it
+}
+
+func (fp *FilePath) AsFlag(message string) *FilePath {
+	fp.flagValue = flag.String(fp.name, "", message)
+	return fp
+}
+
+func (fp *FilePath) Value() string {
+	return fp.response
 }
 
 // Usage is an overwritable function that is used to print command line usage help
 var Usage = func() {
 	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", flag.CommandLine.Name())
 	fmt.Fprintf(flag.CommandLine.Output(), "  %s [options]", filepath.Base(strings.ReplaceAll(flag.CommandLine.Name(), "\\", "/")))
-	for _, ei := range expectedItems {
+	for _, ei := range expectedArgs {
+		if ei.flagValue != nil {
+			continue
+		}
 		fmt.Fprintf(flag.CommandLine.Output(), " [%s]", ei.name)
 	}
 	fmt.Fprintf(flag.CommandLine.Output(), "\n\n")
 	flag.PrintDefaults()
-	for _, ei := range expectedItems {
+	for _, ei := range expectedArgs {
+		if ei.flagValue != nil {
+			continue
+		}
 		fmt.Fprintf(flag.CommandLine.Output(), "  [%s]\n        %s (default \"%s\")\n", ei.name, ei.message, ei.defaultPath)
 	}
 }
 
 // Parse replaces flag.Parse, to add in file selection parsing
 func Parse() {
-	flag.Usage = Usage
 	if !flag.Parsed() {
 		flag.Parse()
 	}
@@ -70,30 +83,39 @@ func Parse() {
 	}
 
 	args := slices.Clone(flag.Args())
-	for i := 0; i < len(expectedItems); i++ {
-		item := &expectedItems[i]
-		if len(args) > i && args[i] != "" {
-			*item.response = args[i]
+	for i, fp := range expectedArgs {
+		if fp.flagValue != nil {
+			args = slices.Insert(args, i, *fp.flagValue)
+			continue
+		}
+		if len(args) <= i {
+			args = append(args, "")
+		}
+	}
+	for i := 0; i < len(expectedArgs); i++ {
+		fp := expectedArgs[i]
+		if args[i] != "" {
+			fp.response = args[i]
 		} else {
 			if !*Quiet {
-				if !getFile(item) {
+				if !getFile(fp) {
 					i-- // retry this
 					continue
 				}
 			}
 		}
 
-		if *item.response == "-" {
+		if fp.response == "-" {
 			continue
 		}
 
-		if item.mustExist {
-			_, err := os.Stat(*item.response)
+		if fp.mustExist {
+			_, err := os.Stat(fp.response)
 			if err != nil {
 				if len(args) > i {
 					args[i] = "" // don't keep retrying this argument - ask for user input
 				}
-				fmt.Fprintf(os.Stderr, "File for %s must exist, but '%s' doesn't\n", item.name, *item.response)
+				fmt.Fprintf(os.Stderr, "File for %s must exist, but '%s' doesn't\n", fp.name, fp.response)
 				if !*Quiet {
 					i-- // retry this
 					continue
@@ -103,20 +125,20 @@ func Parse() {
 	}
 }
 
-func getFile(item *item) bool {
-	fmt.Fprintf(os.Stderr, "%s. %s=[%s]: ", item.message, item.name, item.defaultPath)
+func getFile(fp *FilePath) bool {
+	fmt.Fprintf(os.Stderr, "%s. %s=[%s]: ", fp.message, fp.name, fp.defaultPath)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	text := strings.TrimSpace(scanner.Text())
-	*item.response = text
-	if *item.response == "" {
-		*item.response = item.defaultPath
-		item.defaultPath = "-"
+	fp.response = text
+	if fp.response == "" {
+		fp.response = fp.defaultPath
+		fp.defaultPath = "-"
 		return true
 	}
 
 	if text[0] == '\'' && text[len(text)-1] == '\'' || text[0] == '"' && text[len(text)-1] == '"' {
-		*item.response = (text)[1 : len(text)-1]
+		fp.response = (text)[1 : len(text)-1]
 	}
 	return true
 }
